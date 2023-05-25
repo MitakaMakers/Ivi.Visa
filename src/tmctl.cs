250f81f605c071f2917f1f816a6e504cfb1bdc0a
Mitaka laboratory <mitaka.lab@gmail.com>
2023-05-25 22:48:00 +0900

    update native library

--- src/tmctl.cs
+++ src/tmctl.cs
@@ -1,44 +1,45 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Mm
 {
-    public class tmctl
+    public static class tmctl
     {
-        [DllExport]
-        public static int TmcInitialize(int wire, IntPtr adr, IntPtr id)
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmInitialize(int wire, IntPtr adr, IntPtr id)
         {
             //string str = Marshal.PtrToStringUni(adr);
             //return (IntPtr)str.Length;
             return 1;
         }
 
-        [DllExport]
-        public static int TmcFinish( int id )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmFinish( int id )
         {
             return 2;
         }
 
-        [DllExport]
-        public static int TmcSetTimeout( int id, int tmo )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmSetTimeout( int id, int tmo )
         {
             return 3;
         }
 
-        [DllExport]
-        public static int TmcSetTerm( int id, int eos, int eot )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmSetTerm( int id, int eos, int eot )
         {
             return 4;
         }
 
-        [DllExport]
-        public static int TmcSend( int id, IntPtr msg )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmSend( int id, IntPtr msg )
         {
 //            string str = Marshal.PtrToStringUni(msg);
             return 5;
         }
 
-        [DllExport]
-        public static int TmcReceive( int id, IntPtr buff, int blen, IntPtr rlen )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmReceive( int id, IntPtr buff, int blen, IntPtr rlen )
         {
       //      var str = $"Hello {name}";
 //            Marshal.Copy(buff, rlen)
@@ -47,43 +48,43 @@ namespace Mm
             return 6;
         }
 
-        [DllExport]
-        public static int TmcReceiveOnly( int id, IntPtr buff, int blen, IntPtr rlen )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmReceiveOnly( int id, IntPtr buff, int blen, IntPtr rlen )
         {
             return 7;
         }
 
-        [DllExport]
-        public static int TmcReceiveBlockHeader( int id, IntPtr length )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmReceiveBlockHeader( int id, IntPtr length )
         {
             return 8;
         }
 
-        [DllExport]
-        public static int TmcReceiveBlockData( int id, IntPtr buff, int blen, IntPtr rlen, IntPtr end )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmReceiveBlockData( int id, IntPtr buff, int blen, IntPtr rlen, IntPtr end )
         {
             return 9;
         }
 
-        [DllExport]
-        public static int TmcCheckEnd( int id )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmCheckEnd( int id )
         {
             return 10;
         }
-        [DllExport]
-        public static int TmcGetLastError( int id )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmGetLastError( int id )
         {
             return 11;
         }
 
-        [DllExport]
-        public static int TmcSetRen( int id, int flag )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmSetRen( int id, int flag )
         {
             return 12;
         }
 
-        [DllExport]
-        public static int TmcDeviceClear( int id )
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int TmDeviceClear( int id )
         {
             return 13;
         }
