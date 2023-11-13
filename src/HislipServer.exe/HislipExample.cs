1f3321d94efed1187ccd86834246b047ce82bbb1
Mitaka Laboratory <mitaka.lab@gmail.com>
2023-11-14 04:08:00 +0900

    update source

---
+++ src/HislipServer.exe/HislipExample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Vxi11Net
+{
+    public class HislipListenerExample
+    {
+        public static void Main(string[] args)
+        {
+            HislipServer server = new HislipServer();
+            server.Port = 4880;
+            server.MaximumMessageSize = 1024 * 1024;
+            server.Start();
+            Console.WriteLine("Listening...");
+            HislipListener listener = server.AcceptClient();
+
+            String text;
+            bool IsQuery = false;
+            while (listener.IsListening)
+            {
+                try
+                {
+                    HislipListenerContext context = listener.GetMessage();
+                    HislipListenerRequest request = context.Request;
+                    HislipListenerResponse response = context.Response;
+                    switch (request.MessageType)
+                    {
+                        case Hislip.AsyncLock:
+                            break;
+                        case Hislip.Data_:
+                            text = request.PayloadAsString;
+                            if (text.Contains("?"))
+                            {
+                                IsQuery = true;
+                            }
+                            break;
+                        case Hislip.DataEnd:
+                            text = request.PayloadAsString;
+                            if (text.Contains("?"))
+                            {
+                                IsQuery = true;
+                            }
+                            if (IsQuery)
+                            {
+
+                                response.ControlCode = Hislip.RMTwasDelivered;
+                                byte[] data = System.Text.Encoding.ASCII.GetBytes("XYZCO,246B,S000-0123-02,0");
+                                response.Payload = data;
+                                byte[] bytes = response.GetBytes();
+                                response.OutputStream.Write(bytes);
+                            }
+                            break;
+                        case Hislip.DeviceClearComplete:
+                            break;
+                        case Hislip.AsyncRemoteLocalControl:
+                            break;
+                        case Hislip.Trigger:
+                            break;
+                        case Hislip.AsyncDeviceClear:
+                            break;
+                        case Hislip.AsyncServiceRequest:
+                            break;
+                        case Hislip.AsyncStatusQuery:
+                            break;
+                        case Hislip.AsyncLockInfo:
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
--- src/Vxi11Server.exe/Vxi11Example.cs
+++ src/Vxi11Server.exe/Vxi11Example.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using TmctlAPINet;
 
 namespace Vxi11Net
 {
@@ -9,10 +8,6 @@ namespace Vxi11Net
     {
         public static void Main2(string[] args)
         {
-            TMCTL tmctl = new TMCTL();
-            int id = 0;
-            tmctl.Initialize(TMCTL.TM_CTL_USBTMC2, "TEMP01", ref id);
-
             Vxi11Server server = new Vxi11Server();
             server.Start();
             Console.WriteLine("Listening...");
@@ -31,12 +26,10 @@ namespace Vxi11Net
                         case Vxi11.DEVICE_WRITE:
                             StreamReader reader = new StreamReader(request.InputStream);
                             String text = reader.ReadToEnd();
-                            tmctl.Send(id, text);
                             break;
                         case Vxi11.DEVICE_READ:
                             StringBuilder buff = new StringBuilder(1000);
                             int rlen = 0;
-                            tmctl.Receive(id, buff, 1000, ref rlen);
                             if (rlen > 0)
                             {
                                 Stream output = response.OutputStream;
